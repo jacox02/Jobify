55e25a0f371db22697314c9cde3b1bdec7b1ca0a
rafae <rafae@LAPTOP-KFK91717>
2021-01-29 15:25:38 -0400

    colocando estilos a el login

--- src/Components/LoginComponent.js
+++ src/Components/LoginComponent.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import {Form, Button} from "react-bootstrap";
+import "../style/styleLogin.css";
 
 export default class LoginComponent extends Component {
   
   render() {
     return (
-    
-    <Form className="container w-50 mt-5 shadow-none p-3 mb-5 bg-light rounded">
+    <Form className=" formulario container shadow-none p-3 mb-5  rounded" >
     <Form.Group controlId="formBasicEmail">
       <Form.Label>Email address</Form.Label>
       <Form.Control type="email" placeholder="Enter email" />
@@ -20,11 +20,10 @@ export default class LoginComponent extends Component {
       <Form.Control type="password" placeholder="Password" />
     </Form.Group>
     <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-    <Button className="me-md-2" variant="primary" type="submit">
+    <Button className="me-md-2 btn-block" variant="info" type="submit">
       Submit
     </Button>
     </div>
-    
   </Form>
      ) }
 }
