d491e00673f967adc4512e44a4b2da113cad185b
jacox02 <anonimo4671@hotmail.com>
2021-01-30 05:54:50 -0400

    Resubiendo los cambios para resolver el error de la pagina principal

--- API/Routes/works.js
+++ API/Routes/works.js
@@ -4,31 +4,21 @@ const app = express();
 const connection = require("../Database/database");
 
 app.get("/Works/List", (req, res) => {
-  connection.connect();
   connection.query(
     "select * from Categories C, Works W, Companies A where W.Company_ID = A.Company_ID AND W.Category_ID = C.Category_ID",
     (err, results) => {
       if (err) throw err;
-      res.send({
-        message: "Getting works",
-        code: 200,
-        data: results,
-      });
+      res.send(results);
     }
   );
 });
 
 app.get("/Works/:id/Details", (req, res) => {
-  connection.connect();
   connection.query(
     `select * from Categories C, Works W, Companies A where W.Company_ID = A.Company_ID AND W.Category_ID = C.Category_ID and Work_ID = ${req.params.id}`,
     (err, results) => {
       if (err) throw err;
-      res.send({
-        message: "Getting works",
-        code: 200,
-        data: results,
-      });
+      res.send(results);
     }
   );
 });
--- src/Components/JobsComponent.js
+++ src/Components/JobsComponent.js
@@ -1,36 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 
 const axios = require("axios");
 
 export default function Jobs() {
+  const [Works, setWorks] = useState([]);
+
   const getJobs = () => {
     axios
       .get("http://localhost:3050/Works/List")
       .then((response) => {
-        console.log(response);
+        console.log(response.data);
+        setWorks(response.data);
+        console.log(Works);
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
+  const renderJobs = () => {
+    return (
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            Card Subtitle
+          </Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+    );
+  };
+
   useEffect(() => {
     getJobs();
-  });
-  //Terminar de renderizar las tarjetas con un MAP ya el API manda data al frente
+  }, []);
+
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
+    <div>
+      {() => {
+        renderJobs();
+      }}
+    </div>
   );
 }
