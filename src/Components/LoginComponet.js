ff338ba3eff3dfdd0549a41f605573b14d8ceaa1
rafae <rafae@LAPTOP-KFK91717>
2021-02-11 21:08:23 -0400

    new component Login

---
+++ src/Components/LoginComponet.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Form, Button,} from "react-bootstrap";
+
+export default function LoginComponet() {
+    return (
+        <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+    )
+}
