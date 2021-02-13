b2707b9ecc8e0a8a019e2ed75fed95986df46184
rafae <rafae@LAPTOP-KFK91717>
2021-02-13 12:43:37 -0400

    Estilo a el formulario de postulacion

--- src/Components/PostulateComponent.js
+++ src/Components/PostulateComponent.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button, Row, Col } from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
+import "../style/stylePostulate.css"
 
 export default function PostulateComponent() {
   const { user, isAuthenticated } = useAuth0();
   return (
     isAuthenticated && (
       <div>
-        <Form>
+        <Form className="FormPostulate">
           <Form.Group>
             <Form.Label>Email</Form.Label>
             <Form.Control type="text" value={user.email} />
@@ -20,6 +21,13 @@ export default function PostulateComponent() {
             <Form.Label>why do you deserve the job?</Form.Label>
             <Form.Control as="textarea" rows={3} />
           </Form.Group>
+          <Form.Group as={Row}>
+          <Col>
+            <Button variant="secondary"  block type="submit">
+              Publicar
+            </Button>
+          </Col>
+        </Form.Group>
         </Form>
       </div>
     )
