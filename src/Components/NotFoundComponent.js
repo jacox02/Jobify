901d37192256ff6251cc4c861db2bcae03c81443
rafae <rafae@LAPTOP-KFK91717>
2021-03-12 16:02:28 -0400

    estilo a el error 404 y limpiando archivos

--- src/Components/NotFoundComponent.js
+++ src/Components/NotFoundComponent.js
@@ -1,30 +1,10 @@
 import React from "react";
-
+import error404 from "../img/404.png";
+import "../style/404ComponentStyles.css";
 export default function NotFoundComponent() {
   return (
     <div>
-      <figure>
-        <div class="sad-mac"></div>
-        <figcaption>
-          <span class="sr-text">Error 404: Not Found</span>
-          <span class="e"></span>
-          <span class="r"></span>
-          <span class="r"></span>
-          <span class="o"></span>
-          <span class="r"></span>
-          <span class="_4"></span>
-          <span class="_0"></span>
-          <span class="_4"></span>
-          <span class="n"></span>
-          <span class="o"></span>
-          <span class="t"></span>
-          <span class="f"></span>
-          <span class="o"></span>
-          <span class="u"></span>
-          <span class="n"></span>
-          <span class="d"></span>
-        </figcaption>
-      </figure>
+       <img className="error404" src={error404}/>
     </div>
   );
 }
